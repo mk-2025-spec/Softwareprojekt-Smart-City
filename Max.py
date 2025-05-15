ced8d94cc52beaed1b0305f27b99521071b420ee
mk-2025-spec <maximilian.krueper@web.de>
2025-05-15 09:06:00 +0200

    Update Max.py

--- Max.py
+++ Max.py
@@ -1,9 +1,23 @@
-def smartcity():
-    print("behoben")
+def Programm():
+    Autor = "Max Mustermann"
+    Datum = "15.05.2025"
+    Problem = "Stein Straße"
+    Status = "in Bearbeitung"
     
+    burger = [Autor, Datum,Problem, Status]
+    print(str(burger))
     
-smartcity()
+    eingabe = input("Gib eine neue Information ein: ")  # Speichert die Eingabe des Nutzers
+    burger.append(eingabe)  # Fügt die Eingabe zur Liste hinzu
+    
+    print("Aktualisierte Daten:", burger)
 
+Programm()
+
+        
+
+
+        
 
 
 

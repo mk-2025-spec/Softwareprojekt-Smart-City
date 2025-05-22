0d01a42dd4ccdde54870488bd2e184686a8f56a5
mk-2025-spec <maximilian.krueper@web.de>
2025-05-22 09:14:37 +0200

    Update Max.py

--- Max.py
+++ Max.py
@@ -1,406 +1,73 @@
 def Programm():
-    Autor = "JOCHEN MANTERROCHEN"
-    Datum = "15.05.3025"
-    Problem = "Stein Straße"
-    Status = "in Bearbeitung"
-    
-    burger = [Autor, Datum,Problem, Status]
-    print(str(burger))
-    
-    eingabe = input("Gib eine neue Information ein: ") 
-    burger.append(eingabe)  
-    
-    print("Aktualisierte Daten:", burger)
-    print(burger)
+   
+    Autor1 = "Anna Müller"
+    Datum1 = "10.04.3025"
+    Problem1 = "Brücke beschädigt"
+    Status1 = "Abgeschlossen"
 
-
-    Problem1 = "Bank Kaputt"
+    Autor2 = "Peter Schmidt"
+    Datum2 = "21.03.3025"
     Problem2 = "Straßenlaterne defekt"
-    Problem3 = "Müll nicht abgeholt"
+    Status2 = "Gemeldet"
 
-    Probleme = [Problem1,Problem2,Problem3]
+    Autor3 = "Maria Keller"
+    Datum3  = "05.02.3025"
+    Problem3 = "Wasserschaden im Tunnel"
+    Status3 = "In Bearbeitung"
+
+    Autor4 = "Lukas Bauer"
+    Datum4 = "22.06.3025"
+    Problem4 = "Baum blockiert Straße"
+    Status4 = "Gemeldet"
+
+    Autor5 = "Sophie Wagner"
+    Datum5 = "30.07.3025"
+    Problem5 = "Ampel ausgefallen"
+    Status5 = "In Bearbeitung"
+
+    Autor6 = "Maximilian Hoffmann"
+    Datum6 = "12.08.3025"
+    Problem6 = "Schlagloch auf Hauptstraße"
+    Status6 = "Abgeschlossen"
+
+    Autor7 = "Elena Fischer"
+    Datum7 = "05.09.3025"
+    Problem7 = "Ölspur auf Fahrbahn"
+    Status7 = "Gemeldet"
+    
+    Autor8 = "JOCHEN MANTERROCHEN"
+    Datum8 = "15.05.3025"
+    Problem8 = "Stein Straße"
+    Status8 = "in Bearbeitung"
+    
+    burger1 = [Autor1, Datum1,Problem1, Status1]
+    burger2 = [Autor2, Datum2,Problem2, Status2]
+    burger3 = [Autor3, Datum3,Problem3, Status3]
+    burger4 = [Autor4, Datum4, Problem4, Status4]
+    burger5 = [Autor5, Datum5, Problem5, Status5]
+    burger6 = [Autor6, Datum6, Problem6, Status6]
+    burger7 = [Autor7, Datum7, Problem7, Status7]
+    burger8 = [Autor8, Datum8,Problem8, Status8]
     
 
+    
+    #eingabe = input("Gib eine neue Information ein: ") 
+    #burger.append(eingabe)  
+    
+    
+    print("Burger 1:", burger1)
+    print("Burger 2:", burger2)
+    print("Burger 3:", burger3)
+    print("Burger 4:", burger4)
+    print("Burger 5:", burger5)
+    print("Burger 6:", burger6)
+    print("Burger 7:", burger7)
+    print("Burger 8:", burger8)
+    
 Programm()       
 
 
         
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
